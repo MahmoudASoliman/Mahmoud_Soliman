eb3712db8ab2d2cd4a924d4609bc131bd995a0b7
MahmoudASoliman <mahmoudahmed8692@gmail.com>
2025-08-31 22:46:01 +0300

    chore: add Vercel config & build script

---
+++ vercel-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# نزّل Flutter (stable) وأضفه للـ PATH
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PWD/flutter/bin:$PATH"
+
+flutter --version
+flutter config --enable-web
+flutter pub get
+
+# إبني نسخة الويب
+flutter build web --release
